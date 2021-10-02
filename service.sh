09a944bc134255d6ec38d695a2e20c12df339372
Abaiik <nurifvan07@gmail.com>
2021-10-02 21:38:33 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -8,7 +8,25 @@ echo 0 > /proc/cpufreq/cpufreq_imax_thermal_protect
 echo 1 > /sys/module/ged/parameters/boost_gpu_enable
 echo 1 > /sys/module/ged/parameters/gx_force_cpu_boost
 echo 1 > /sys/module/ged/parameters/boost_extra
+echo 1 >/sys/module/ged/parameters/gx_game_mode
+echo 1 >/sys/module/ged/parameters/gx_force_cpu_boost
+echo 1 > /sys/module/ged/parameters/boost_amp
+echo 1 > /sys/module/ged/parameters/enable_cpu_boost
+echo 1 > /sys/module/ged/parameters/enable_gpu_boost
+echo 1 > /sys/module/ged/parameters/enable_game_self_frc_detect
+echo 0 > /sys/module/ged/parameters/gpu_idle
 
+# Touch Sampling
+echo Enabling Game Touch Sampling Boost
+echo 1 > /proc/touchpanel/game_switch_enable
+
+# CPU Load settings
+echo 0-7 > /dev/cpuset/foreground/cpus
+echo 0-3 > /dev/cpuset/background/cpus
+echo 0-7 > /dev/cpuset/system-background/cpus
+echo 0-7 > /dev/cpuset/top-app/cpus
+echo 0 > /dev/cpuset/restricted/cpus
+    
 # 5GB Zram
 swapoff /dev/block/zram0
 echo 1 > /sys/block/zram0/reset
